cc0480795e86c84699965120e5def47c23cff931
Riley <rmp00003@mix.wvu.edu>
2022-01-21 13:42:31 -0500

    Lab 1: TopNavBar and BottomNavBar Implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
+import { TopNavBarComponent } from 'src/app/header/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { BottomNavBarComponent } from './footer/bottomnavbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    BottomNavBarComponent,
   ],
   imports: [
     BrowserModule
